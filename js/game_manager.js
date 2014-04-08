76f11a5e905395e37750c1ff4590371771e9bde0
acslocum <acslocum@thoughtworks.com>
2014-04-08 15:36:04 -0400

    256 is so much easier than 2048

--- js/game_manager.js
+++ js/game_manager.js
@@ -170,8 +170,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 2048 tile. Ish.
+          if (merged.value === 256) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
